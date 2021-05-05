bb649b6e4cde5dc499dddc508b4ee547669df417
DevEagle <80274309+DevEagleIsTaken@users.noreply.github.com>
2021-05-05 15:14:55 -0400

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+num1 = input('Enter First Number: ')
+num2 = input('Enter Second Number: ')
+
+result1 = float(num1) + float(num2)
+result2 = float(num1) - float(num2)
+result3 = float(num1) * float(num2)
+result4 = float(num1) / float(num2)
+result5 = float(num1) ** float(num2)
+
+print('Addition: ', result1)
+print('Subtraction: ',result2)
+print('Multiplication: ',result3)
+print('Division: ',result4)
+print('Powers: ',result5)
